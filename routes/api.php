9f32f14c3bf4c9b3e82cf76afd9704f6540dce45
LeonelCeron <ceronleonel99@gmail.com>
2024-09-30 20:39:13 -0600

    API Routes Setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refrehToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
 });
